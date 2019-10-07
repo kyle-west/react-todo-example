c878de0e389045404708679bc82ca040161fcee1
Kyle West <kyle@familysearch.org>
2019-10-07 17:56:44 -0600

    [STEP 4] modify state from user input

--- src/App.js
+++ src/App.js
@@ -7,18 +7,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Super Cool Todo App</h1>
-      <TodoList 
-        items={{
-          // [label]: isChecked 
-          "Test Todo feature 1": true,
-          "Test Todo feature 2": false,
-          "Test Todo feature 3": true,
-          "Test Todo feature 4": false,
-          "Test Todo feature 5": false,
-          "Test Todo feature 6": false,
-        }}
-      />
+      <h1>✅ Super Cool Todo App</h1>
+      <TodoList />
     </div>
   );
 }
