1374514764918d919b6daf16049df364a7e6db85
Kyle West <kyle@familysearch.org>
2019-10-07 16:57:14 -0600

    [STEP 2] render todo items

--- src/components/TodoList/index.js
+++ src/components/TodoList/index.js
@@ -6,11 +6,12 @@ import TodoItem from '../TodoItem'
 export default function TodoList ({}) {
   return (
     <ul className="TodoList">
-      <TodoItem />
-      <TodoItem />
-      <TodoItem />
-      <TodoItem />
-      <TodoItem />
+      <TodoItem label="Test Todo feature 1" checked />
+      <TodoItem label="Test Todo feature 2" />
+      <TodoItem label="Test Todo feature 3" checked />
+      <TodoItem label="Test Todo feature 4" />
+      <TodoItem label="Test Todo feature 5" />
+      <TodoItem label="Test Todo feature 6" />
     </ul>
   )
 }
