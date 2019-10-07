7c8aaffe907910337e25e17895887eff43b688af
Kyle West <kyle@familysearch.org>
2019-10-07 16:57:14 -0600

    [STEP 2] render todo items

--- src/components/TodoItem/index.js
+++ src/components/TodoItem/index.js
@@ -1,8 +1,19 @@
 import React from 'react'
 import './styles.css'
 
-export default function TodoItem({}) {
+export default function TodoItem({ label, checked }) {
+
+
+
   return (
-    <li className="TodoItem">TODO</li>
+    <li 
+      className="TodoItem" 
+      
+      // reflect to an attribute in the DOM - for styling
+      data-checked={checked ? '' : undefined}
+    >
+      <input type="checkbox" checked={!!checked} />
+      {label}
+    </li>
   )
 }
